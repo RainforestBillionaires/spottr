993eee13d5c8bca262009b867cf8377f6cac33cc
bryson189 <bryson189@gmail.com>
2017-12-02 13:27:49 -0600

    Fixes formatting issues

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -9,18 +9,21 @@ module.exports = function (app, db) {
         return console.log(err);
       if (user == undefined) {
         bcrypt.hash(req.body.password, constants.SALT_ROUNDS, function(err, hash) {
-        var query = new db.User({
-            email: req.body.email,
-            firstName: req.body.fName,
-            lastName: req.body.lName
-            phoneNumber: req.body.phoneNum,
-            address: req.body.address,
-            password: hash
+          var query = new db.User({
+              email: req.body.email,
+              firstName: req.body.fName,
+              lastName: req.body.lName,
+              phoneNumber: req.body.phoneNum,
+              address: req.body.address,
+              password: hash
+          });
+          query.save();
         });
-        query.save();
-      else {
-        res.json("A user with the specified email already exists.");
       }
+      else {
+        res.status(404).send("A user with the specified email already exists.");
+      }
+    });
   });
 
   app.post('/api/signIn', function (req, res) {
