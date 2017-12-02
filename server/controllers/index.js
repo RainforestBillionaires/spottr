f49bf0b24fb58c01bc0fd31d3a37f51746e40ad2
bryson189 <bryson189@gmail.com>
2017-12-02 12:53:53 -0600

    Changes spacing indent to 2

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,5 @@
 module.exports = function(app, db){
-    require ('./ads')(app, db);
-    require ('./users')(app, db);
-    require ('./transactions')(app, db);
+  require ('./ads')(app, db);
+  require ('./users')(app, db);
+  require ('./transactions')(app, db);
 }
--- server/controllers/users.js
+++ server/controllers/users.js
@@ -1,6 +1,13 @@
 module.exports = function (app, db) {
   app.post('/api/createUser', function (req, res) {
-
+    var query = new db.User({
+        email: req.body.email,
+        name: req.body.email,
+        phoneNumber: req.body.email,
+        address: req.body.email,
+        password: req.body.password
+    });
+    query.save();
   });
 
   app.post('/api/signIn', function (req, res) {
