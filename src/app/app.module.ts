648235f139b799aad9434ba56f86fa87f145c0b1
Nabil <nabillionaire@live.com>
2017-12-02 11:28:05 -0600

    Bootstrapping

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-import { AdDetailsComponent } from './ad-details/ad-details.component';
-import { AdListItemComponent } from './ad-list-item/ad-list-item.component';
-import { AdListViewComponent } from './ad-list-view/ad-list-view.component';
+import { AdDetailsComponent } from './components/ad-details/ad-details.component';
+import { AdListItemComponent } from './components/ad-list-item/ad-list-item.component';
+import { AdListViewComponent } from './components/ad-list-view/ad-list-view.component';
 import { LoginScreenComponent } from './components/login-screen/login-screen.component';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
