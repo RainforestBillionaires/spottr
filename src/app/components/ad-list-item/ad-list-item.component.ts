df4f8d60c3253f9e3c142aa95fbb31e9be6d6f54
Nabil <nabillionaire@live.com>
2017-12-02 20:43:43 -0600

    Final commit

--- src/app/components/ad-list-item/ad-list-item.component.ts
+++ src/app/components/ad-list-item/ad-list-item.component.ts
@@ -10,15 +10,6 @@ export class AdListItemComponent implements OnInit {
   @Input() ad: Advertisement;
   clicked: Boolean = false;
 
-  // ad: Advertisement = {
-  //   pictures: ["/assets/images/lawnmower.jpg", "/assets/images/lawnmower2.jpg"],
-  //   title: "Lawnmower for rent",
-  //   description: "Pristine lawnmower looking to be borrowed. DM me.Pristine lawnmower looking to be borrowed. DM me.Pristine lawnmower looking to be borrowed. DM me.Pristine lawnmower looking to be borrowed. DM me.Pristine lawnmower looking to be borrowed. DM me.",
-  //   location: "3620 rue Lorne-Crescent, Montreal Quebec, Canada",
-  //   value: 70,
-  //   userId: "nabil.chowdhury@outlook.com"
-  // };
-
   constructor() { }
 
   clickedAd() {
